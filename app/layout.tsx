0a851ec4cf5529e2632b3f6cee556958fc1d4127
Nondef Ined <acesino25@gmail.com>
2025-06-27 09:29:44 -0300

    fix: apariencia

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Dynamiksoft | Convertimos los desafíos en soluciones de software escalables',
+  description: 'Dynamiksoft desarrolla software a medida para resolver desafíos reales. Equipos interdisciplinarios que convierten ideas en soluciones.',
+  generator: 'dynamiksoft',
+  icons:{
+    icon: '/favicon.ico',
+  }
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  )
+}
